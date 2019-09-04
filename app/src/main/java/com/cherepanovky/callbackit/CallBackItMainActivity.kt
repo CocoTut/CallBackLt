de054e18d7a876311ed19f8ca8b536084174994e
cherepanovkj <cherepanovkj@gmail.com>
2019-09-04 21:42:38 +0300

    add settings stub

--- app/src/main/java/com/cherepanovky/callbackit/CallBackItMainActivity.kt
+++ app/src/main/java/com/cherepanovky/callbackit/CallBackItMainActivity.kt
@@ -5,8 +5,10 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.findNavController
 import com.cherepanovky.callbackit.di.FeatureProxyInjector
+import kotlinx.android.synthetic.main.activity_route.*
 import ru.cherepanovk.core.di.ComponentManager
 import ru.cherepanovk.core.platform.BaseActivity
+import ru.cherepanovk.feature_settings_impl.SettingsFeatureStarterImpl
 
 class CallBackItMainActivity : BaseActivity() {
 
@@ -17,6 +19,11 @@ class CallBackItMainActivity : BaseActivity() {
         setContentView(R.layout.activity_route)
         val navGraph = FeatureProxyInjector.getEventsFeature().eventsFeatureStarter().getNavGraph( navController.navInflater)
         navController.graph = navGraph
+
+        btn.setOnClickListener {
+           val settingsGraph =  SettingsFeatureStarterImpl.getNavGraph( navController.navInflater)
+            navController.graph = settingsGraph
+        }
     }
 
     override fun inject(componentManager: ComponentManager) {
---
+++ feature_settings_impl/src/main/java/ru/cherepanovk/feature_settings_impl/SettingsFragment.kt
@@ -0,0 +1,15 @@
+package ru.cherepanovk.feature_settings_impl
+
+import ru.cherepanovk.core.di.ComponentManager
+import ru.cherepanovk.core.platform.BaseFragment
+
+class SettingsFragment : BaseFragment(R.layout.item_event) {
+    override fun inject(componentManager: ComponentManager) {
+    }
+
+    override fun bindListeners() {
+    }
+
+    override fun bindViewModel() {
+    }
+}
