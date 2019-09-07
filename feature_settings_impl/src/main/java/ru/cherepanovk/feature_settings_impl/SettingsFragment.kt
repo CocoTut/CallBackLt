22822bcce3f011e0d1c471f2085d6b4297380ad2
cherepanovkj <cherepanovkj@gmail.com>
2019-09-07 14:18:01 +0300

    add navigation

--- feature_settings_impl/src/main/java/ru/cherepanovk/feature_settings_impl/SettingsFragment.kt
+++ feature_settings_impl/src/main/java/ru/cherepanovk/feature_settings_impl/SettingsFragment.kt
@@ -3,7 +3,7 @@ package ru.cherepanovk.feature_settings_impl
 import ru.cherepanovk.core.di.ComponentManager
 import ru.cherepanovk.core.platform.BaseFragment
 
-class SettingsFragment : BaseFragment(R.layout.item_event) {
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun inject(componentManager: ComponentManager) {
     }
 
