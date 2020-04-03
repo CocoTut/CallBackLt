1dcf672522e1ef9a07b8538fb099e5e9557837f5
Cherepanovkj <cherepanovkj@gmal.com>
2020-04-03 23:34:09 +0300

    change ViewModel binding

--- core/src/main/java/ru/cherepanovk/core/utils/extentions/Fragment.kt
+++ core/src/main/java/ru/cherepanovk/core/utils/extentions/Fragment.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.ViewModelProvider.Factory
 import androidx.lifecycle.ViewModelProviders
 import android.content.Context
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import ru.cherepanovk.core.platform.BaseFragment
 
 inline fun <reified T : ViewModel> Fragment.viewModel(factory: Factory, body: T.() -> Unit): T {
-    val vm = ViewModelProviders.of(this, factory)[T::class.java]
+    val vm = ViewModelProvider(this, factory).get(T::class.java)
     vm.body()
     return vm
 }
